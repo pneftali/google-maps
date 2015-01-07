c479586c06dda4016531bd90ffb81ca85390d12b
pneftali <papelleras.nef@gmail.com>
2015-01-08 00:52:26 +0800

    added places.js and enhancements

---
+++ places.js
@@ -0,0 +1,351 @@
+var restaurant_list = [
+{
+  'location' : new google.maps.LatLng(10.312502, 123.91138000000001),
+  'place_id' : 'ChIJl39-B2uZqTMR8BVI5hon62M',
+  'name' : "Carlito's Way Diner",
+  'revenue_total' : 120000,
+  'type' : 1 // pizza house
+},
+{
+  'location' : new google.maps.LatLng(10.318976, 123.91201799999999),
+  'place_id' : 'ChIJxzzAPBSZqTMR1BGreCE562k',
+  'name' : "Neo Neo's Grill House",
+  'revenue_total' : 10000,
+  'type' : 1 // pizza house
+},
+{
+  'location' : new google.maps.LatLng(10.314214, 123.91811600000005),
+  'place_id' : 'ChIJsRmUtBKZqTMRavlJsEC-9Fw',
+  'name' : "House of Pizza",
+  'revenue_total' : 90000,
+  'type' : 1 // pizza house
+},
+{
+  'location' : new google.maps.LatLng(10.312185, 123.91885500000001),
+  'place_id' : 'ChIJZdhRe22ZqTMRs-XccUq1vkc',
+  'name' : "Gerry's Grill - SM Cebu",
+  'revenue_total' : 130000,
+  'type' : 1 // pizza house
+},        
+{
+  'location' : new google.maps.LatLng(10.31227, 123.91860300000008),
+  'place_id' : 'ChIJoXEMpm2ZqTMRI_NcNsND0LQ',
+  'name' : "Golden Cowrie - SM City Cebu",
+  'revenue_total' : 122000,
+  'type' : 1 // pizza house
+},
+{
+  'location' : new google.maps.LatLng(10.309734, 123.91776099999993),
+  'place_id' : 'ChIJC4iQqW2ZqTMRKOaqhijbMt4',
+  'name' : "CnT Lechon",
+  'revenue_total' : 123000,
+  'type' : 1 // pizza house
+},
+{
+  'location' : new google.maps.LatLng(10.310457, 123.91824399999996),
+  'place_id' : 'ChIJZdhRe22ZqTMRbaw-2UqMJ5w',
+  'name' : "Manila Hearing Aid - Cebu",
+  'revenue_total' : 110000,
+  'type' : 1 // pizza house
+},
+{
+  'location' : new google.maps.LatLng(10.32021, 123.91228699999999),
+  'place_id' : 'ChIJP7qrohaZqTMRi3QH9uOS4i8',
+  'name' : "iamik's Chicken & Br",
+  'revenue_total' : 190000,
+  'type' : 1 // pizza house
+},
+{
+  'location' : new google.maps.LatLng(10.311485, 123.91724999999997),
+  'place_id' : 'ChIJt1DepW2ZqTMRJE2F06NuZKU',
+  'name' : "Mcdonald'S Philippines",
+  'revenue_total' : 180000,
+  'type' : 1 // pizza house
+},
+{
+  'location' : new google.maps.LatLng(10.31429, 123.91476699999998),
+  'place_id' : 'ChIJpb52ShOZqTMRC2_ElbMpD_8',
+  'name' : "Andoks Litson- Mabolo",
+  'revenue_total' : 10000,
+  'type' : 1 // pizza house
+},
+{
+  'location' : new google.maps.LatLng(10.312304, 123.91899699999999),
+  'place_id' : 'ChIJcWUSY22ZqTMR2cbxUHYbcFs',
+  'name' : "Rai Rai Ken",
+  'revenue_total' : 125000,
+  'type' : 2 // fast food
+},
+{
+  'location' : new google.maps.LatLng(10.309088, 123.91758000000004),
+  'place_id' : 'ChIJ_a0AdG6ZqTMRnElK7VeMIpc',
+  'name' : "Red Carpet Catering Services",
+  'revenue_total' : 220000,
+  'type' : 2 // fast food
+},
+{
+  'location' : new google.maps.LatLng(10.31161, 123.917552),
+  'place_id' : 'ChIJEYwnoW2ZqTMRWiisWonfpX8',
+  'name' : "Pizza Hut",
+  'revenue_total' : 210000,
+  'type' : 2 // fast food
+},
+{
+  'location' : new google.maps.LatLng(10.312439, 123.91930100000002),
+  'place_id' : 'ChIJtbRNe22ZqTMRVjUVz819lq4',
+  'name' : "Spice Fusion",
+  'revenue_total' : 160000,
+  'type' : 2 // fast food
+},
+{
+  'location' : new google.maps.LatLng(10.311485, 123.91724999999997),
+  'place_id' : 'ChIJt1DepW2ZqTMR68lXxZ1LpSM',
+  'name' : "Majestic Chinese Cuisine",
+  'revenue_total' : 20000,
+  'type' : 2 // fast food
+},
+{
+  'location' : new google.maps.LatLng(10.316748, 123.91307400000005),
+  'place_id' : 'ChIJ3UEdnhSZqTMRp9OaWa5Auq4',
+  'name' : "Biaños Pizzaderia - Mabolo Branch",
+  'revenue_total' : 40000,
+  'type' : 2 // fast food
+},
+{
+  'location' : new google.maps.LatLng(10.311927, 123.91848600000003),
+  'place_id' : 'ChIJwdpOd22ZqTMRRourAF5jWqE',
+  'name' : "Classic Savory",
+  'revenue_total' : 120060,
+  'type' : 2 // fast food
+},
+{
+  'location' : new google.maps.LatLng(10.31957, 123.91126299999996),
+  'place_id' : 'ChIJ-SV6KRSZqTMRRIYUHd5LvME',
+  'name' : "Corner Table",
+  'revenue_total' : 124000,
+  'type' : 2 // fast food
+},
+{
+  'location' : new google.maps.LatLng(10.318776, 123.90590900000007),
+  'place_id' : 'ChIJzXNlXD6ZqTMRVnTIpSHIBu0',
+  'name' : "Laguna Garden Cafe",
+  'revenue_total' : 120500,
+  'type' : 2 // fast food
+},
+{
+  'location' : new google.maps.LatLng(10.325096, 123.90455999999995),
+  'place_id' : 'ChIJWcPeLiOZqTMRq7mUXoHWS8M',
+  'name' : "Port Restaurant",
+  'revenue_total' : 120600,
+  'type' : 2 // fast food
+},
+{
+  'location' : new google.maps.LatLng(10.319617, 123.89911200000006),
+  'place_id' : 'ChIJPTKTpDmZqTMRNBOwcrVHv70',
+  'name' : "Harolds Hotel Cebu",
+  'revenue_total' : 120200,
+  'type' : 3 // grilled
+},
+{
+  'location' : new google.maps.LatLng(10.318025, 123.90506200000004),
+  'place_id' : 'ZqTMRlVvdTTuR9cA',
+  'name' : "Gerry'S Grill",
+  'revenue_total' : 120340,
+  'type' : 3 // grilled
+},
+{
+  'location' : new google.maps.LatLng(10.320283, 123.89912500000003),
+  'place_id' : 'ChIJ47mpkDmZqTMRewken2CpivA',
+  'name' : "Handuraw Pizza Events Café",
+  'revenue_total' : 120540,
+  'type' : 3 // grilled
+},
+{
+  'location' : new google.maps.LatLng(10.31279, 123.898371),
+  'place_id' : 'ChIJD8RHEkaZqTMR6Oh2a4Y6AOU',
+  'name' : "Kentucky Fried Chicken",
+  'revenue_total' : 120110,
+  'type' : 3 // grilled
+},
+{
+  'location' : new google.maps.LatLng(10.321958, 123.91303500000004),
+  'place_id' : 'ChIJhUeB1xaZqTMRRo1KHvOoN74',
+  'name' : "Wakamatsu Yakiniku Japanese",
+  'revenue_total' : 120270,
+  'type' : 3 // grilled
+},
+{
+  'location' : new google.maps.LatLng(10.31319, 123.89011400000004),
+  'place_id' : 'ChIJwcXXjUuZqTMRJdZ8Q-hQZvI',
+  'name' : "STK ta Bay!",
+  'revenue_total' : 120340,
+  'type' : 3 // grilled
+},
+{
+  'location' : new google.maps.LatLng(10.309457, 123.891705),
+  'place_id' : 'ChIJB8Pbdk6ZqTMRM7vtzNdasrs',
+  'name' : "Larsian BBQ",
+  'revenue_total' : 120120,
+  'type' : 3 // grilled
+},
+{
+  'location' : new google.maps.LatLng(10.31745, 123.89416099999994),
+  'place_id' : 'ChIJAQCw4UmZqTMR5yEv1Q0i8-A',
+  'name' : "Zubuchon",
+  'revenue_total' : 120180,
+  'type' : 3 // grilled
+},
+{
+  'location' : new google.maps.LatLng(10.315051, 123.89093400000002),
+  'place_id' : 'ChIJwbiLLEqZqTMRInRs274yzIE',
+  'name' : "Kul Kitchen",
+  'revenue_total' : 120180,
+  'type' : 3 // grilled
+},
+{
+  'location' : new google.maps.LatLng(10.307057, 123.89729499999999),
+  'place_id' : 'ChIJz-mQVkWZqTMR3GuWXwu1oPE',
+  'name' : "Jo's Chicken Inato",
+  'revenue_total' : 120180,
+  'type' : 3 // grilled
+},
+{
+  'location' : new google.maps.LatLng(10.329087, 123.90751499999999),
+  'place_id' : 'ChIJa3KRRiKZqTMRTxLSjba7SAw',
+  'name' : "ilaputi",
+  'revenue_total' : 121400,
+  'type' : 4 // sea food
+},
+{
+  'location' : new google.maps.LatLng(10.329517, 123.90977399999997),
+  'place_id' : 'ChIJp2mNyhiZqTMRxU7Ju0goKgY',
+  'name' : "Café Georg",
+  'revenue_total' : 120110,
+  'type' : 4 // sea food
+},
+{
+  'location' : new google.maps.LatLng(10.327807, 123.90986999999996),
+  'place_id' : 'ChIJC_0M7hiZqTMRhibLe8imS0M',
+  'name' : "Maya Mexican Restaurant",
+  'revenue_total' : 121100,
+  'type' : 4 // sea food
+},
+{
+  'location' : new google.maps.LatLng(10.328867, 123.909222),
+  'place_id' : 'ChIJGbxuvBiZqTMR5_wAQAvCpss',
+  'name' : "The Tinderbox",
+  'revenue_total' : 111000,
+  'type' : 4 // sea food
+},
+{
+  'location' : new google.maps.LatLng(10.306586, 123.89710400000001),
+  'place_id' : 'ChIJuxeeqVqZqTMRvfvljTrcoek',
+  'name' : "Beehive",
+  'revenue_total' : 124100,
+  'type' : 4 // sea food
+},
+{
+  'location' : new google.maps.LatLng(10.298273, 123.89819799999998),
+  'place_id' : 'ChIJm0hqE1iZqTMRIYc8SnvL4_A',
+  'name' : "Our Place Restaurant and Bar",
+  'revenue_total' : 120190,
+  'type' : 4 // sea food
+},
+{
+  'location' : new google.maps.LatLng(10.298824, 123.90316699999994),
+  'place_id' : 'ChIJ30plJ-KbqTMRRiMipocYSiM',
+  'name' : "Cafe Elysa",
+  'revenue_total' : 121800,
+  'type' : 4 // sea food
+},
+{
+  'location' : new google.maps.LatLng(10.305562, 123.89786200000003),
+  'place_id' : 'ChIJ1SCrl1qZqTMRVfXq10AXOjQ',
+  'name' : "Ipar's Restaurante Y Bar De Tapas",
+  'revenue_total' : 120190,
+  'type' : 4 // sea food
+},
+{
+  'location' : new google.maps.LatLng(10.304873, 123.89508599999999),
+  'place_id' : 'ChIJ6az0WFCZqTMRF2CGYcV7qXA',
+  'name' : "7-Eleven",
+  'revenue_total' : 120190,
+  'type' : 4 // sea food
+},
+{
+  'location' : new google.maps.LatLng(10.298326, 123.89552100000003),
+  'place_id' : 'ChIJgxuqw1eZqTMRc96LhO7VSkQ',
+  'name' : "Red Ribbon",
+  'revenue_total' : 121900,
+  'type' : 4 // sea food
+},
+{
+  'location' : new google.maps.LatLng(10.292923, 123.89875200000006),
+  'place_id' : 'ChIJG5EXEOObqTMRV9tXXzc_mGM',
+  'name' : "Cebu Lumpia House",
+  'revenue_total' : 120190,
+  'type' : 5 // sea food
+},
+{
+  'location' : new google.maps.LatLng(10.300462, 123.898188),
+  'place_id' : 'ChIJL60vx1mZqTMRTQXyovt_IxY',
+  'name' : "Orange Brutus",
+  'revenue_total' : 110000,
+  'type' : 5 // italian
+},
+{
+  'location' : new google.maps.LatLng(10.295343, 123.89942900000005),
+  'place_id' : 'ChIJGbm08OKbqTMROBgBdE3KUqU',
+  'name' : "Manila Foodshoppe",
+  'revenue_total' : 120090,
+  'type' : 5 // italian
+},
+{
+  'location' : new google.maps.LatLng(10.295417, 123.892564),
+  'place_id' : 'ChIJo_NSP1WZqTMRkQD9U8vx5UE',
+  'name' : "Papa'S Roast & Seafood",
+  'revenue_total' : 120110,
+  'type' : 5 // italian
+},
+{
+  'location' : new google.maps.LatLng(10.298522, 123.89731600000005),
+  'place_id' : 'ChIJ4f9qIliZqTMRb5jNmw3ACyM',
+  'name' : "Mang Inasal Restaurant",
+  'revenue_total' : 120110,
+  'type' : 5 // italian
+},
+{
+  'location' : new google.maps.LatLng(10.321771, 123.92917499999999),
+  'place_id' : 'ChIJWZzeDQiZqTMRdwTopiVL_WM',
+  'name' : "MO2 Restobar",
+  'revenue_total' : 121110,
+  'type' : 5 // italian
+},
+{
+  'location' : new google.maps.LatLng(10.325261, 123.933535),
+  'place_id' : 'ChIJw6b4C6mZqTMRkMFj538cqO8',
+  'name' : "TsiBoom Native Restaurant",
+  'revenue_total' : 121210,
+  'type' : 5 // italian
+},
+{
+  'location' : new google.maps.LatLng(10.32197, 123.93213300000002),
+  'place_id' : 'ChIJEQ8t36eZqTMR_jxHZAhE7s8',
+  'name' : "Bubble Bee Tea House",
+  'revenue_total' : 121210,
+  'type' : 5 // italian
+},
+{
+  'location' : new google.maps.LatLng(10.32197, 123.93213300000002),
+  'place_id' : 'ChIJydYbOamZqTMRe2w1g3vAr6g',
+  'name' : "Choobi Choobi Parkmall",
+  'revenue_total' : 121210,
+  'type' : 5 // italian
+},
+{
+  'location' : new google.maps.LatLng(10.325208, 123.93513699999994),
+  'place_id' : 'ChIJoaxwDKmZqTMRb1wODt9yh8Q',
+  'name' : "Persian Kebab Tandoori",
+  'revenue_total' : 121210,
+  'type' : 5 // italian
+}];
